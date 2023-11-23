e2682fd3df2df2418e15b19daa82090d4b17b9d1
bknvpik <b.knvpik@gmail.com>
2023-11-23 21:57:18 +0100

    Add component states, implement player rounds

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
-export default function Square({ value, onSquareClick }) {
+import { useState } from "react";
+import Square from "./components/Square";
+import "./styles.css";
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const players = {
+    x: "X",
+    o: "O",
+  };
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+
+    xIsNext ? (nextSquares[i] = players.x) : (nextSquares[i] = players.o);
+
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
   return (
-    <button
-      className="square"
-      onClick={onSquareClick}
-    >
-      {value}
-    </button>
+    <>
+      <div className='board-row'>
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+        />
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+        />
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+        />
+      </div>
+      <div className='board-row'>
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+        />
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+        />
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+        />
+      </div>
+      <div className='board-row'>
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+        />
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+        />
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+        />
+      </div>
+    </>
   );
 }
